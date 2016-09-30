816f4c7342e8aded2aefad30fbda9b7a94339e55
Sean Smith <scsmithr@gmail.com>
2016-09-30 10:01:21 -0400

    Add vim config

--- link.sh
+++ link.sh
@@ -1,4 +1,12 @@
+#!/bin/env bash
 # Symlink files to where they're expected to be found
 
+DIR="$(pwd)"
+
 # Tmux
-ln -sv ./tmux/.tmux.conf ~
+ln -sv $DIR/tmux/tmux.conf ~/.tmux.conf
+
+# vim
+ln -sv $DIR/vim/vimrc ~/.vimrc
+mkdir -p ~/.vim/colors
+ln -sv $DIR/vim/custom-base16-dark.vim ~/.vim/colors
