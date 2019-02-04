72c72c71df85bbba8569908b8fea1efc211b56b2
Sean Smith <scsmithr@gmail.com>
2019-02-04 09:55:01 -0600

    Actually don't like bold

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -13,8 +13,8 @@ precmd() {
 }
 
 zstyle ':vcs_info:*' check-for-changes false # set true to show unstaged
-zstyle ':vcs_info:git*' formats "%{$fg_bold[green]%}(%b%u)%{$reset_color%}"
-zstyle ':vcs_info:git*' actionformats "%{$fg_bold[green]%}(%b%u) $fg_bold[red][%a]%{$reset_color%}"
+zstyle ':vcs_info:git*' formats "%{$fg[green]%}(%b%u)%{$reset_color%}"
+zstyle ':vcs_info:git*' actionformats "%{$fg[green]%}(%b%u) $fg[red][%a]%{$reset_color%}"
 zstyle ':vcs_info:*' unstagedstr "*"
 
 # Display current vcs status. Single quotes to delay eval
@@ -23,15 +23,15 @@ PROMPT_VCS='${vcs_info_msg_0_}'
 # Display username@host during ssh sessions.
 PROMPT_HOST=""
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-    PROMPT_HOST="%{$fg_bold[cyan]%}%n@%m%{$reset_color%} "
+    PROMPT_HOST="%{$fg[cyan]%}%n@%m%{$reset_color%} "
 fi
 
 # Display current dir, center truncating if path is long.
-PROMPT_DIR="%{$fg_bold[blue]%}%(5~|%-1~/.../%3~|%4~)%{$reset_color%}"
+PROMPT_DIR="%{$fg[blue]%}%(5~|%-1~/.../%3~|%4~)%{$reset_color%}"
 
 # Display vim mode on keymap change.
 function zle-line-init zle-keymap-select {
-    PROMPT_VIM_MODE="%{$fg_bold[yellow]%} -- N --%{$reset_color%}"
+    PROMPT_VIM_MODE="%{$fg[yellow]%} -- N --%{$reset_color%}"
     if [ "$KEYMAP" = vicmd ]; then
         echo -ne "\e[1 q"
         RPS1=$PROMPT_VIM_MODE
@@ -45,7 +45,7 @@ zle -N zle-line-init
 zle -N zle-keymap-select
 
 PROMPT="
-%${PROMPT_TRUNC_WIDTH}>...>$PROMPT_HOST$PROMPT_DIR %(1j.$fg_bold[yellow](%j) $reset_color.)$PROMPT_VCS%>>
+%${PROMPT_TRUNC_WIDTH}>...>$PROMPT_HOST$PROMPT_DIR %(1j.$fg[yellow](%j) $reset_color.)$PROMPT_VCS%>>
 %{%(?.$fg[magenta].$fg[yellow])%}>%{$reset_color%} "
 
 RPROMPT=""
