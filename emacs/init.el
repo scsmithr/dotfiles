a03cb5b555b94c8121cbed9c341f2f5c8d798409
Sean Smith <scsmithr@gmail.com>
2019-05-01 17:43:16 -0500

    Idk what I'm doing

--- emacs/init.el
+++ emacs/init.el
@@ -133,7 +133,7 @@
 
 (global-set-key (kbd "C-S-o") 'ido-goto-symbol) ; or any key you see fit
 
-(
+(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
