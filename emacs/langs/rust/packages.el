f1cf7f3c9cebf71e6de6b6711eb9a96a04407217
Sean Smith <scsmithr@gmail.com>
2019-05-12 14:50:52 -0500

    Remove company-capf from backends in rust mode

--- emacs/langs/rust/packages.el
+++ emacs/langs/rust/packages.el
@@ -6,6 +6,10 @@
       :defer t
       :config
       (setq rust-format-on-save t)
+      ;; See https://github.com/tigersoldier/company-lsp/issues/61
+      (add-hook 'rust-mode-hook
+          (lambda () (setq company-backends
+                           (delete 'company-capf company-backends))))
       (add-hook 'rust-mode-hook #'lsp)))
 
 (provide 'rust)
