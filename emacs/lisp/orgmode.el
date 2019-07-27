6c348030bc3d25b69dcba871d6192a8268c3caf9
Sean Smith <scsmithr@gmail.com>
2019-07-27 15:06:11 -0500

    Add some org templates

--- emacs/lisp/orgmode.el
+++ emacs/lisp/orgmode.el
@@ -5,16 +5,35 @@
 ;; Where org-captures go.
 (setq org-default-notes-file "~/notes/refile.org")
 
+(setq org-agenda-files (list "~/notes/"))
+(setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+
+(setq org-capture-templates
+      '(
+        ("n" "Note" entry (file+headline "" "Notes")
+         "* %?
+  %U
+  %i
+  %a"
+         )
+        ("t" "Task" entry (file+headline "" "Tasks")
+         "* TODO %?
+  %U
+  %a"
+         )
+        ))
+
+
 (setq org-startup-folded nil)
 (setq org-hide-leading-stars t)
 
 (after! org
-   (org-babel-do-load-languages
-    'org-babel-load-languages
-    (append org-babel-load-languages
-            '((shell . t)
-              (restclient . t)
-              (http . t)))))
+        (org-babel-do-load-languages
+         'org-babel-load-languages
+         (append org-babel-load-languages
+                 '((shell . t)
+                   (restclient . t)
+                   (http . t)))))
 
 (use-package ob-restclient
   :ensure t)
