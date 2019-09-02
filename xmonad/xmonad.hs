2890ffc8076dd49de1a4fecc0692aee72a99328e
Sean Smith <scsmithr@gmail.com>
2019-09-02 13:44:39 -0500

    Colors

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -222,7 +222,6 @@ primary = "#268bd2"
 urgent = "#cb4b16"
 
 -- config vars
--- myFocusedBorderColor = "#96A7A9"
 myFocusedBorderColor = "#c6c6c6"
 myUnfocusedBorderColor = "#F2E6CE"
 myBorderWidth = 1
