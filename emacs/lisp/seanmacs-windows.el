e85e5e433c0f0b58e37ac0e97f143b7b138d5b0f
Sean Smith <scsmithr@gmail.com>
2019-10-15 10:43:24 -0500

    Select windows after splitting

--- emacs/lisp/seanmacs-windows.el
+++ emacs/lisp/seanmacs-windows.el
@@ -6,14 +6,26 @@
 ;;; Code:
 
 (core/leader
-   "ww" 'split-window-right
-   "wh" 'split-window-below
+   "ww" 'seanmacs/split-window-right
+   "wh" 'seanmacs/split-window-below
    "wH" 'shrink-window-horizontally
    "wL" 'enlarge-window-horizontally
    "wK" 'shrink-window
    "wJ" 'enlarge-window
    "w=" 'balance-windows
    "." 'repeat)
+
+(defun seanmacs/split-window-right ()
+  "Split window to the right, selecting it."
+  (interactive)
+  (let ((window (split-window-right)))
+    (select-window window)))
+
+(defun seanmacs/split-window-below ()
+  "Split window below current, selecting it."
+  (interactive)
+  (let ((window (split-window-below)))
+    (select-window window)))
 
 (use-package winner
   :config
