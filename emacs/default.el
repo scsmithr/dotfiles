9a74ed60f18e74da56321c2e330c1a87ffc4c52e
Sean Smith <scsmithr@gmail.com>
2019-12-26 15:58:56 -0500

    Update kube package

--- emacs/default.el
+++ emacs/default.el
@@ -49,7 +49,7 @@
  ("json-reformat" . "8eb6668ed447988aea06467ba8f42e1f2178246f")
  ("json-snatcher" . "c4cecc0a5051bd364373aa499c47a1bb7a5ac51c")
  ("julia-emacs" . "d21b83db56ae74d232dc2be2cd87810c5b8a6451")
- ("kube.el" . "485f7d9f4102367551c9e95f78fe2fd9325d78ad")
+ ("kube.el" . "df0585ae59751f0d2a902ab897be53cbabd241c5")
  ("let-alist" . "57561d7d9267da48e8b7ab1f3bd45c9d05951289")
  ("lsp-mode" . "47484c4969b467c3dddfbd073efa655d1fcc6983")
  ("lsp-ui" . "e2b124dc677cf9e5a1c1a383e61edc1f136b284a")
