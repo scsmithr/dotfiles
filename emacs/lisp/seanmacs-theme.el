b40d0ffe8dc745d2c0c2330f1a3e9ce3aabe456a
Sean Smith <scsmithr@gmail.com>
2022-01-05 11:41:12 -0600

    Add back weight properties except for variable-pitch

--- emacs/lisp/seanmacs-theme.el
+++ emacs/lisp/seanmacs-theme.el
@@ -8,12 +8,12 @@
 (defun sm/setup-fonts (&optional frame)
   "Setup fonts for FRAME."
   (message "Setting fonts")
-  (set-face-attribute 'default nil :family "Triplicate A" :height 120)
+  (set-face-attribute 'default nil :family "Triplicate A" :height 120 :weight 'normal)
   (set-face-attribute 'variable-pitch nil :family "Source Serif Pro" :height 120)
   ;; Unicode fallbacks.
-  (set-fontset-font t 'unicode (font-spec :name "Triplicate A"))
+  (set-fontset-font t 'unicode (font-spec :name "Triplicate A" :weight 'normal))
   (set-fontset-font t 'unicode (font-spec :name "JuliaMono" :weight 'light) nil 'append)
-  (set-fontset-font t 'unicode (font-spec :name "DejaVu Sans") nil 'append))
+  (set-fontset-font t 'unicode (font-spec :name "DejaVu Sans" :weight 'normal) nil 'append))
 
 (sm/setup-fonts)
 ;; Setup fonts on every frame load so that the unicode fontsets get set
