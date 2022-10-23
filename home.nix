673db10aa44809f3917ec3c81a6a9a30a3337851
Sean Smith <scsmithr@gmail.com>
2022-10-23 12:07:52 -0500

    More configuration

--- home.nix
+++ home.nix
@@ -6,7 +6,23 @@
   home.stateVersion = "22.05";
   programs.home-manager.enable = true;
 
+  home.sessionPath = [
+    "$HOME/.bin/"
+    "$HOME/.cargo/bin"
+    "$HOME/.elan/bin"
+    "$HOME/\${config.programs.go.goPath}/bin"
+    # Stack installs binaries here. Needed since I'm using stack to manage
+    # xmonad.
+    "$HOME/.local/bin"
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "editor";
+    JULIA_NUM_THREADS = "16";
+  };
+
   # Packages
+
   home.packages = with pkgs; [
     # Misc
     htop
@@ -22,6 +38,9 @@
     ripgrep
     tree
 
+    # Google cloud
+    google-cloud-sdk
+
     # Go
     go
     gopls
@@ -31,7 +50,15 @@
     rustc
     rustfmt
     rust-analyzer
+
+    # Javascript/Typescript
+    yarn
+    nodejs-16_x
   ];
+
+  programs.firefox = {
+    enable = true;
+  };
 
   programs.alacritty = {
     enable = true;
@@ -40,7 +67,6 @@
     };
   };
 
-  # Emacs
   home.file.".emacs.d" = {
     source = ./emacs;
     recursive = true;
@@ -48,5 +74,48 @@
 
   programs.emacs = {
     enable = true;
+  };
+
+  programs.bash = {
+    enable = true;
+    # Be consistent so eshell regex can match this.
+    bashrcExtra = "PS1='[\u@\h \W]\$ '";
+  };
+
+  programs.go = {
+    enable = true;
+    goPath = ".go";
+  };
+
+  programs.git = {
+    enable = true;
+    userEmail = "scsmithr@gmail.com";
+    userName = "Sean Smith";
+    signing = {
+      key = "BA3E3A399960AD0D";
+      signByDefault = true;
+    };
+    attributes = [
+      "*.org diff=org"
+      "*.md diff=md"
+    ];
+    ignores = [
+      "worktree/"
+      "vendor/"
+      "node_modules/"
+      ".log/"
+      ".ccls-cache/"
+    ];
+    extraConfig = {
+      core = {
+        editor = "editor";
+      };
+      github = {
+        user = "scsmithr";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+    };
   };
 }
