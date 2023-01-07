f4d46b1c19b100ceeabf97853c07eb282ca6718c
Sean Smith <scsmithr@gmail.com>
2023-01-06 22:24:47 -0600

    Remove most theme customizations

--- emacs/lisp/seanmacs-theme.el
+++ emacs/lisp/seanmacs-theme.el
@@ -47,110 +47,20 @@
   (minions-mode 1))
 
 (use-package modus-themes
-  :straight (:host sourcehut :repo "protesilaos/modus-themes" :branch "version-4")
+  :straight (:host sourcehut :repo "protesilaos/modus-themes" :branch "main")
   :config
 
   (setq modus-themes-italic-constructs nil
         modus-themes-bold-constructs nil
-        modus-themes-region '(bg-only)
         modus-themes-org-blocks 'gray-background
-        modus-themes-prompts '(bold))
+        modus-themes-prompts '(italic))
 
   (setq modus-themes-common-palette-overrides
         '(
-          (fringe bg-inactive)
-          (cursor fg-main)
-          (builtin fg-main)
-          (comment yellow-cooler)
-          (constant fg-main)
-          (docstring green-warmer)
-          (docmarkup green-cooler)
-          (fnname fg-main)
-          (keyword fg-dim)
-          (preprocessor magenta-faint)
-          (string blue-faint)
-          (type fg-main)
-          (variable fg-main)
-          (rx-construct fg-main)
-          (rx-backslash fg-main)
-
-          (keybind blue-cooler)
-          (fg-link blue-faint)
-          (fg-link-symbolic cyan-faint)
-          (fg-link-visited magenta-faint)
-          (name magenta)
-          (identifier yellow-cooler)
-          (prompt blue-faint)
-
-          (err red)
-          (warning yellow-warmer)
-          (info green)
-
-          (underline-err red-intense)
-          (underline-warning yellow-intense)
-          (underline-note cyan-intense)
-
-          (fg-accent-0 blue-faint)
-          (fg-accent-1 magenta-faint)
-          (fg-accent-2 cyan-faint)
-          (fg-accent-3 red-faint)
-          (bg-accent-0 bg-blue-subtle)
-          (bg-accent-1 bg-magenta-subtle)
-          (bg-accent-2 bg-cyan-subtle)
-          (bg-accent-3 bg-red-subtle)
-
-          (date-common cyan-faint)
-          (date-deadline red)
-          (date-event fg-alt)
-          (date-holiday magenta)
-          (date-scheduled yellow-warmer)
-          (date-weekend red-faint)
-
-          (mail-cite-0 blue-faint)
-          (mail-cite-1 yellow-warmer)
-          (mail-cite-2 cyan-cooler)
-          (mail-cite-3 red-cooler)
-          (mail-part cyan)
-          (mail-recipient magenta-cooler)
-          (mail-subject magenta-warmer)
-          (mail-other magenta-faint)
-
-          (prose-block fg-dim)
-          (prose-code green-warmer)
-          (prose-done green-warmer)
-          (prose-macro magenta-warmer)
-          (prose-metadata fg-dim)
-          (prose-metadata-value fg-alt)
-          (prose-table fg-alt)
-          (prose-tag magenta-faint)
-          (prose-todo red-warmer)
-          (prose-verbatim magenta-warmer)
-
-          (fg-heading-0 fg-main)
-          (fg-heading-1 fg-main)
-          (fg-heading-2 fg-main)
-          (fg-heading-3 fg-main)
-          (fg-heading-4 fg-main)
-          (fg-heading-5 fg-main)
-          (fg-heading-6 fg-main)
-          (fg-heading-7 fg-main)
-          (fg-heading-8 fg-main)
-
-          ;; Mode line
-          (bg-mode-line-active        bg-active)
-          (fg-mode-line-active        fg-main)
-          (border-mode-line-active    border)
-          (bg-mode-line-inactive      bg-inactive)
-          (fg-mode-line-inactive      fg-dim)
-          (border-mode-line-inactive  border)
+          (fg-region unspecified)
+          (bg-region bg-ochre)
           ))
 
-  (setq modus-vivendi-palette-overrides
-        '(
-          (bg-main          "#292626")
-          (bg-dim           "#353333")
-          (fg-dim           "#b8b8b8")
-          ))
 
   (defun sm/customize-modus ()
     (modus-themes-with-colors
@@ -163,8 +73,7 @@
        `(whitespace-space ((t (:foreground ,bg-dim :background unspecified))))
        `(whitespace-tab ((t (:foreground ,bg-dim :background unspecified))))
        ;; Region (unspecify distant foreground)
-       `(region ((,c :distant-foreground unspecified
-                     ,@(modus-themes--region bg-region fg-main bg-region-subtle))))
+       `(region ((t :distant-foreground unspecified :background ,bg-region :foreground ,fg-region)))
        ;; Eglot
        `(eglot-highlight-symbol-face ((t :background ,bg-hover-secondary :weight unspecified)))
        `(eglot-diagnostic-tag-deprecated-face ((t :inherit unspecified)))
